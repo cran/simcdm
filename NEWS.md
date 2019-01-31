104b9c4b1dfa46477a55867c2e9492b454b40962
James Joseph Balamuta <balamut2@illinois.edu>
2019-01-31 21:10:03 +0000

    version 0.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,24 @@
+# simcdm 0.1.0
+
+## Changes
+
+- Renamed `sim_attribute_classes()` to `attribute_classes()`.
+- Addressed ambiguous calls to `std::pow(<int>, <int>)`
+
+## Documentation
+
+- Added a `CITATION` file for the package
+- Improved Vignette Examples
+- Improved README contents
+
+## Deployment
+
+- Added Unit Tests to verify simulation routines and attribute generations.
+- Added testing on Travis-CI for the previous release of _R_, e.g. the `oldrel`.
+
 # simcdm 0.0.5
+
+## Features
 
 - Added _C++_ and _R_ functions for simulation of:
     - Deterministic Input, Noisy "And" Gate (DINA)
@@ -9,5 +29,8 @@
     - Matrices:
         - Random Q Matrix: `sim_q_matrix()`
         - ETA Matrix: `sim_eta_matrix()`
-        - Latent Attribute Profile Matrix: `sim_attribute_classes()`
         - Latent Attribute Profiles for Subjects: `sim_subject_attributes()`
+    - Attributes:
+        - Latent Attribute Profile Matrix: `sim_attribute_classes()`
+        - Attribute Bijection: `attribute_bijection()`
+        - Attribute Inverse Bijection: `attribute_inv_bijection()`
--- inst/doc/overview-simcdm.R
+++ inst/doc/overview-simcdm.R
@@ -1,7 +1,7 @@
-## ------------------------------------------------------------------------
+## ----load-r-pkg----------------------------------------------------------
 library(simcdm)
 
-## ------------------------------------------------------------------------
+## ----setup-matrix-sims---------------------------------------------------
 # Set a seed for reproducibility
 set.seed(888)
 
@@ -10,26 +10,39 @@ N = 15   # Number of Examinees / Subjects
 J = 10   # Number of Items
 K = 2    # Number of Skills / Attributes
 
-## ------------------------------------------------------------------------
+## ----sim-q-matrix--------------------------------------------------------
+# Set a seed for reproducibility
+set.seed(1512)
+
+# Simulate an identifiable Q matrix
 Q = sim_q_matrix(J, K)
 Q
 
-## ------------------------------------------------------------------------
+## ----sim-eta-matrix------------------------------------------------------
+# Set a seed for reproducibility
+set.seed(4421)
+
+# Simulate an eta matrix
 eta = sim_eta_matrix(K, J, Q)
 eta
 
-## ------------------------------------------------------------------------
-class_alphas = sim_attribute_classes(K)
+## ----attribute-classes-gen-----------------------------------------------
+# Create a listing of all attribute classes 
+class_alphas = attribute_classes(K)
 class_alphas
 
-## ------------------------------------------------------------------------
+## ----sim-subject-attributes----------------------------------------------
+# Set a seed for reproducibility
+set.seed(5126)
+
+# Create attributes for a subject 
 subject_alphas = sim_subject_attributes(N, K)
 subject_alphas
 
 # Equivalent to:
 # subject_alphas = class_alphas[sample(2 ^ K, N, replace = TRUE),]
 
-## ------------------------------------------------------------------------
+## ----setup-sim-dina------------------------------------------------------
 # Set a seed for reproducibility
 set.seed(888)
 
@@ -47,15 +60,23 @@ Q = sim_q_matrix(J, K)
 # Simulate subject attributes
 subject_alphas = sim_subject_attributes(N, K)
 
-## ------------------------------------------------------------------------
+## ----sim-dina-items------------------------------------------------------
+# Set a seed for reproducibility
+set.seed(2019)
+
+# Simulate items under the DINA model
 items_dina = sim_dina_items(subject_alphas, Q, ss, gs)
 items_dina
 
-## ------------------------------------------------------------------------
+## ----sim-dina-attributes-------------------------------------------------
+# Set a seed for reproducibility
+set.seed(51823)
+
+# Simulate attributes under the DINA model
 attributes = sim_dina_attributes(subject_alphas, Q)
 attributes
 
-## ------------------------------------------------------------------------
+## ----rrum-sim-setup------------------------------------------------------
 # Set a seed for reproducibility
 set.seed(888)
 
@@ -68,6 +89,9 @@ K = 2    # Number of Skills / Attributes
 # who do not lack any required attribute
 pistar = rep(.9, J)
 
+# Simulate an identifiable Q matrix
+Q = sim_q_matrix(J, K)
+
 # Penalties for failing to have each of the required attributes
 rstar  = .5 * Q
 
@@ -78,10 +102,14 @@ pis = c(.1, .2, .3, .4)
 subject_alphas = sim_subject_attributes(N, K, prob = pis)
 
 # Equivalent to:
-# class_alphas = sim_attribute_classes(K)
+# class_alphas = attribute_classes(K)
 # subject_alphas = class_alphas[sample(2 ^ K, N, replace = TRUE, prob = pis),]
 
-## ------------------------------------------------------------------------
+## ----sim-rrum------------------------------------------------------------
+# Set a seed for reproducibility
+set.seed(912)
+
+# Generate rRUM items
 rrum_items = sim_rrum_items(Q, rstar, pistar, subject_alphas)
 rrum_items
 
